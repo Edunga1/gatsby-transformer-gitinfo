d91e52bfcf80684a73f5ca780e331200e0a41edb
edunga1 <goonr21@gmail.com>
2024-03-29 19:29:45 +0000

    Refactor test

--- src/__tests__/gatsby_node.ts
+++ src/__tests__/gatsby_node.ts
@@ -1,55 +1,36 @@
-import fs from 'fs'
-import os from 'os'
-import path from 'path'
-import git, { SimpleGit } from 'simple-git'
-import { onCreateNode } from '../gatsby-node'
-import { jest, beforeEach, describe, expect, it } from '@jest/globals'
-
-interface Node {
-  absolutePath: string
-  dir: string
-  id: string
-  parent: null
-  children: []
-  internal: {
-    type: string
-  }
-}
-
-interface Actions {
-  createNodeField: jest.Mock
-}
-
-interface CreateNodeSpec {
-  node: Node
-  actions: Actions
-}
+import fs from "fs"
+import os from "os"
+import path from "path"
+import { Actions, Node } from "gatsby"
+import git, { SimpleGit } from "simple-git"
+import { onCreateNode } from "../gatsby-node"
+import { jest, beforeEach, describe, expect, it } from "@jest/globals"
 
 let createNodeField: jest.Mock
-let actions: Actions
 let node: Node
-let createNodeSpec: CreateNodeSpec
+let createNodeSpec: { node: Node, actions: Partial<Actions> }
 let dummyRepoPath: string
 let dummyOtherRepoPath: string
 
 beforeEach(() => {
   createNodeField = jest.fn()
-  actions = { createNodeField }
 
   node = {
-    absolutePath: `/some/path/file.mdx`,
-    dir: `/some/path`,
-    id: `whatever`,
+    absolutePath: "/some/path/file.mdx",
+    dir: "/some/path",
+    id: "whatever",
     parent: null,
     children: [],
     internal: {
+      owner: "",
       type: "File",
+      contentDigest: "",
     },
   }
 
   createNodeSpec = {
     node,
-    actions,
+    actions: { createNodeField },
   }
 })
 
@@ -65,29 +46,29 @@ const initGitRepo = async (repoPath: string, username: string, useremail: string
   return gitRepo
 }
 
-describe(`Processing nodes not matching initial filtering`, () => {
-  it(`should not add any field when internal type is not 'File'`, async () => {
+describe("Processing nodes not matching initial filtering", () => {
+  it('should not add any field when internal type is not "File"', async () => {
     node.internal.type = "Other"
-    await onCreateNode(createNodeSpec)
+    await onCreateNode!(createNodeSpec)
     expect(createNodeField).not.toHaveBeenCalled()
   })
 
-  it(`should not add any field when full path is not in include`, async () => {
-    await onCreateNode(createNodeSpec, {
+  it("should not add any field when full path is not in include", async () => {
+    await onCreateNode!(createNodeSpec, {
       include: /notmatching/,
     })
     expect(createNodeField).not.toHaveBeenCalled()
   })
 
-  it(`should not add any field when full path is in ignore`, async () => {
-    await onCreateNode(createNodeSpec, {
+  it("should not add any field when full path is in ignore", async () => {
+    await onCreateNode!(createNodeSpec, {
       ignore: /some\/path\/file/,
     })
     expect(createNodeField).not.toHaveBeenCalled()
   })
 
-  it(`should not add any field when full path is in include and in ignore`, async () => {
-    await onCreateNode(createNodeSpec, {
+  it("should not add any field when full path is in include and in ignore", async () => {
+    await onCreateNode!(createNodeSpec, {
       include: /mdx/,
       ignore: /some\/path\/file/,
     })
@@ -95,7 +76,7 @@ describe(`Processing nodes not matching initial filtering`, () => {
   })
 })
 
-describe(`Processing File nodes matching filter regex`, () => {
+describe("Processing File nodes matching filter regex", () => {
   beforeEach(async () => {
     dummyRepoPath = fs.mkdtempSync(
       path.join(os.tmpdir(), "gatsby-transform-gitinfo-")
@@ -140,7 +121,7 @@ describe(`Processing File nodes matching filter regex`, () => {
   it("should add log and remote git info to commited File node", async () => {
     node.absolutePath = `${dummyRepoPath}/README.md`
     node.dir = dummyRepoPath
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /md/,
       dir: dummyRepoPath,
     })
@@ -164,7 +145,7 @@ describe(`Processing File nodes matching filter regex`, () => {
 
   it("should add log and remote git info to file from symlink folder", async () => {
     node.absolutePath = `${dummyRepoPath}/symlink/CONTENT.md`
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /md/,
     })
     expect(createNodeField).toHaveBeenCalledTimes(3)
@@ -187,31 +168,31 @@ describe(`Processing File nodes matching filter regex`, () => {
 
   it("should add log and remote git info to file from symlink file", async () => {
     node.absolutePath = `${dummyRepoPath}/CONTENT_LINKED.md`
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /md/,
     })
     expect(createNodeField).toHaveBeenCalledTimes(3)
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestAuthorName`,
-      value: `Some One Else`,
+      name: "gitLogLatestAuthorName",
+      value: "Some One Else",
     })
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestAuthorEmail`,
-      value: `someone@else.com`,
+      name: "gitLogLatestAuthorEmail",
+      value: "someone@else.com",
     })
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestDate`,
-      value: `2018-08-20T21:19:19+00:00`,
+      name: "gitLogLatestDate",
+      value: "2018-08-20T21:19:19+00:00",
     })
   })
 
   it("should not add log or remote git info to unversionned File node", async () => {
     node.absolutePath = `${dummyRepoPath}/unversionned`
     node.dir = dummyRepoPath
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /unversionned/,
       dir: dummyRepoPath,
     })
@@ -219,7 +200,7 @@ describe(`Processing File nodes matching filter regex`, () => {
   })
 })
 
-describe(`Returning the latest matching commit`, () => {
+describe("Returning the latest matching commit", () => {
   beforeEach(async () => {
     dummyRepoPath = fs.mkdtempSync(
       path.join(os.tmpdir(), "gatsby-transform-gitinfo-")
@@ -235,29 +216,29 @@ describe(`Returning the latest matching commit`, () => {
     fs.writeFileSync(`${dummyRepoPath}/README.md`, "Hello")
     await gitRepo.add("README.md")
     await gitRepo.commit(["Add README", "Changing README, with @magictag in body"], "README.md", {
-      "--date": '"Mon 5 Aug 2005 05:05:05 UTC"',
+      "--date": "'Mon 5 Aug 2005 05:05:05 UTC'",
     })
 
     fs.writeFileSync(`${dummyRepoPath}/README.md`, "update1")
     await gitRepo.commit("pickme: update README", "README.md", {
-      "--date": '"Mon 10 Aug 2010 10:10:10 UTC"',
+      "--date": "'Mon 10 Aug 2010 10:10:10 UTC'",
     })
 
     fs.writeFileSync(`${dummyRepoPath}/README.md`, "update2")
     await gitRepo.commit("content: changing README", "README.md", {
-      "--date": '"Mon 15 Aug 2015 15:15:15 UTC"',
+      "--date": "'Mon 15 Aug 2015 15:15:15 UTC'",
     })
 
     fs.writeFileSync(`${dummyRepoPath}/README.md`, "update3")
     await gitRepo.commit("skip: Changing README", "README.md", {
-      "--date": '"Mon 20 Aug 2020 20:20:20 UTC"',
+      "--date": "'Mon 20 Aug 2020 20:20:20 UTC'",
     })
   })
 
   it("should add the latest commit without an inverted match in its log message", async () => {
     node.absolutePath = `${dummyRepoPath}/README.md`
     node.dir = dummyRepoPath
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /md/,
       dir: dummyRepoPath,
       match: {
@@ -268,25 +249,25 @@ describe(`Returning the latest matching commit`, () => {
     expect(createNodeField).toHaveBeenCalledTimes(3)
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestAuthorName`,
-      value: `Some One`,
+      name: "gitLogLatestAuthorName",
+      value: "Some One",
     })
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestAuthorEmail`,
-      value: `some@one.com`,
+      name: "gitLogLatestAuthorEmail",
+      value: "some@one.com",
     })
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestDate`,
-      value: `2015-08-15T15:15:15+00:00`,
+      name: "gitLogLatestDate",
+      value: "2015-08-15T15:15:15+00:00",
     })
   })
 
   it("should add the latest commit with a given match in its log message", async () => {
     node.absolutePath = `${dummyRepoPath}/README.md`
     node.dir = dummyRepoPath
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /md/,
       dir: dummyRepoPath,
       match: {
@@ -314,7 +295,7 @@ describe(`Returning the latest matching commit`, () => {
   it("should add the latest commit with a given match in the log message body", async () => {
     node.absolutePath = `${dummyRepoPath}/README.md`
     node.dir = dummyRepoPath
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /md/,
       dir: dummyRepoPath,
       match: {
@@ -324,25 +305,25 @@ describe(`Returning the latest matching commit`, () => {
     expect(createNodeField).toHaveBeenCalledTimes(3)
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestAuthorName`,
-      value: `Some One`,
+      name: "gitLogLatestAuthorName",
+      value: "Some One",
     })
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestAuthorEmail`,
-      value: `some@one.com`,
+      name: "gitLogLatestAuthorEmail",
+      value: "some@one.com",
     })
     expect(createNodeField).toHaveBeenCalledWith({
       node,
-      name: `gitLogLatestDate`,
-      value: `2005-08-05T05:05:05+00:00`,
+      name: "gitLogLatestDate",
+      value: "2005-08-05T05:05:05+00:00",
     })
   })
 
   it("should not add any nodes if nothing matches a given match", async () => {
     node.absolutePath = `${dummyRepoPath}/README.md`
     node.dir = dummyRepoPath
-    await onCreateNode(createNodeSpec, {
+    await onCreateNode!(createNodeSpec, {
       include: /md/,
       dir: dummyRepoPath,
       match: {
